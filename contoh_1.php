5ca65d1b2e91effb46b067d902b121b20a7c2f80
khoerulrifki <sayarifki8@gmail.com>
2021-09-17 01:53:32 +0700

    first commit

---
+++ contoh_1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh_1 extends CI_Controller{
+    public function index()
+    {
+        echo"<h1> Hai Perkenalkan </h1>";
+        echo"Nama saya Khoerul Rifki <br>
+        Saya tinggal di Jakarta Utara, Tanjung Priok <br>
+        Saya mahasiswa Universitas Bina Sarana Informatika <br>";
+    }
+}
+?>
